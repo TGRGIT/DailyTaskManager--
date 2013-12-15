107b59fb0aa83306bdb03d6b1d48a42d3c3b8af9
Fergus Cooney <ferguscasala@gmail.com>
2013-12-15 13:14:14 +0000

    first

--- esrr++/main.cpp
+++ esrr++/main.cpp
@@ -7,12 +7,29 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+void delay();
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    using namespace std;
+    
+    while(1){
+        cout << "\033[2J\033[1;1H";
+        cout << "Eat" << endl;
+        delay();
+        cout << "Sleep" << endl;
+        delay();
+        cout << "Rave" << endl;
+        delay();
+        cout << "Repeat" << endl;
+        delay();
+    }
     return 0;
+}
+
+void delay(){
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
