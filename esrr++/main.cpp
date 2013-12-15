698b7f38d6ba3c21dbb9969e6d4340b38bd580e3
Fergus Cooney <ferguscasala@gmail.com>
2013-12-15 11:44:06 +0000

    Initial Commit

---
+++ esrr++/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  esrr++
+//
+//  Created by Fergus Cooney on 15/12/2013.
+//  Copyright (c) 2013 Fergus Cooney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
